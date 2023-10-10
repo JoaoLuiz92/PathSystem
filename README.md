070fe2feee41f55e047f3ec2deface0b475bff81
JoaoLuiz <jluizdarosajr@gmail.com>
2023-10-09 21:59:36 -0300

    barra de pesquisa adicionada

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Projeto onde montei um web primeiramente, para organizar situações de validade
 #ideias#
 * fazer campo de filtro para pesquisar clientes.
 * Possibilidade de consulto do proprio cliente ao seus dado e datas de vencimentos.
-**bug imagem na page dos clientes nao renderiza
+**bug imagem na page dos clientes nao renderiza (CORRIGIDO)
--- index.js
+++ index.js
@@ -136,4 +136,18 @@ app.post('/books/remove/:id', function (req, res) {
   })
 })
 
+function search() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase()
+  let x = document.getElementsByClassName('name')
+
+  for (i = 0; i<x.length;i++) {
+    if(!x[i].innerHTML.toLowerCase().includes(input)){
+      x[i].style.display = "none"
+    }else {
+      x[i].style.display = 'list-item'
+    }
+  }
+}
+
 app.listen(3000)
